f3e285783920eb9668ebc7e32277fe3184a29ce2
성희준 <hjsung@uracle.co.kr>
2026-03-12 16:16:59 +0900

    채팅레이아웃 이중스크롤 관련css 제거, 그라데이션 배경 옵션값으로 변경

--- apps/kma-athena/src/widgets/layout/ChatLayout.tsx
+++ apps/kma-athena/src/widgets/layout/ChatLayout.tsx
@@ -8,20 +8,28 @@ interface ChatLayoutProps {
   children: React.ReactNode;
   header?: React.ReactNode;
   showHelp?: boolean;
+  contentBgClassName?: string;
 }
 
 /**
  * 채팅 레이아웃 컴포넌트
  * - 헤더 + 중앙 채팅 영역
  */
-export const ChatLayout = ({ children, header, showHelp = false }: ChatLayoutProps) => {
+export const ChatLayout = ({
+  children,
+  header,
+  showHelp = false,
+  contentBgClassName = 'bg-[linear-gradient(to_top,_#ddf0ff_0%,_transparent_30%)]',
+}: ChatLayoutProps) => {
   return (
     <div className="relative flex flex-col w-full h-full overflow-hidden bg-white">
       <ChatHeader header={header} />
       {showHelp && <HelpGuide />}
 
       <div className="relative flex flex-1 min-h-0 overflow-hidden pt-[56px] md:pt-0">
-        <section className="relative flex-1 min-w-0 overflow-y-auto custom-scrollbar bg-white bg-[linear-gradient(to_top,_#ddf0ff_0%,_transparent_30%)]">
+        <section
+          className={`relative flex-1 min-w-0 overflow-hidden bg-white ${contentBgClassName}`}
+        >
           {children}
         </section>
       </div>
